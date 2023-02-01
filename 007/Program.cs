5e5ad345aefca69f7548973acc4669e46ab3e1e3
Dmitrian23 <dimonio1991@mail.ru>
2023-02-01 04:45:08 +0300

    Задача номер 007

---
+++ 007/Program.cs
@@ -0,0 +1,17 @@
+﻿// Зфдача_№7 Выяснить является ли число чётным.
+
+int n;
+    System.Console.Write("Введите пожалуйста любое число, а я определю чётное оно или нет)):");
+         n=Convert.ToInt32(Console.ReadLine());
+
+bool odd=n%2==0;
+
+if (odd)
+   {
+    System.Console.WriteLine($"Введённое вами число :{n} является чётным)");
+   }
+else
+   {
+    System.Console.WriteLine($"Введённое вами число :{n} является не чётным)");
+   }
+    System.Console.WriteLine("Это так весело, попробуй ещё раз!))");
