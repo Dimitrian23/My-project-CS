db0e6dfdf3b656666a072a956d67d3410a01d1f9
Dmitrian23 <dimonio1991@mail.ru>
2023-02-01 04:43:03 +0300

    Задача номер 002

---
+++ 002/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача_№2 С клавиатуры вводятся два вещественных числа.Проверить является ли одно из них квадротом второго.
+double a,b;
+
+System.Console.Write("Введите значение переменной (a):");
+     a=Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите значение переменной (b):");
+     b=Convert.ToInt32(Console.ReadLine());
+
+
+          if (a*a==b) 
+                  System.Console.WriteLine("Число переменной(b) является квадратом числа переменной(a)");
+        
+          if (b*b==a)   
+                  System.Console.WriteLine("Число переменной(a) является квадратом  числа переменной(b)");
+
+          if (a*a!=b)
+                   System.Console.WriteLine("Число переменной(b) не является квадратом числа переменной(a)");
+      
+          if(b*b!=a)
+                   System.Console.WriteLine("Число переменной(a) не является квадратом числа переменной(b)");
+
+
+System.Console.WriteLine("Попробуйте ещё раз! Запустите команду dotnet run и введите новые данные!");
+
+
+
+      
