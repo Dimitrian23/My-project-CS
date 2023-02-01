675ebf9d9df97a2bc2e9bcb0315027574828236f
Dmitrian23 <dimonio1991@mail.ru>
2023-02-01 04:43:27 +0300

    Задача номер 003

---
+++ 003/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача_№3 С клавиатуры вводим два числа a и b. Найти максимальное из них.
+
+double a,b,max=0;
+
+System.Console.Write("Введите значение переменной для (a):");
+     a=Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите значение переменной для (b):");
+     b=Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+    {
+     max=a;
+      System.Console.Write($"Максимальное значение из двух чисел = {max}");
+    }
+if(b>a)
+    { 
+     max=b;
+      System.Console.Write($"Максимальное значение из двух чисел = {max}");
+    }
+
