cef2ba53fe1770c24f7db164c12d6127db25646f
Dmitrian23 <dimonio1991@mail.ru>
2023-02-01 04:44:45 +0300

    Задача номер 006

---
+++ 006/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача_№6 Написать программу вычисления значения функции y=sin(a)
+double a,y;
+ Console.WriteLine("Вычисление y=sin(a) в радианной мере.");
+System.Console.Write("Введите значение для переменной (a):");
+     a=Convert.ToInt32(Console.ReadLine());
+     Console.Write("Sin введёного вами числа для переменной (a)=");
+Console.Write(y=Math.Sin(a));
