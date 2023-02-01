0a6f19a37e6532b0a29f3310cf0f70bfb5602067
Dmitrian23 <dimonio1991@mail.ru>
2023-02-01 04:44:13 +0300

    Задача номер 005

---
+++ 005/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача_№5 С клавиатуры вводится 3 числа. Найти максимальное из трёх чисел.
+double a,b,c,max=0;
+
+System.Console.Write("Введите значение переменной для (a):");
+     a=Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите значение переменной для (b):");
+     b=Convert.ToInt32(Console.ReadLine());
+
+
+System.Console.Write("Введите значение переменной для (c):");
+     c=Convert.ToInt32(Console.ReadLine());
+max=a;
+if(b>max)
+    { 
+     max=b;
+    }
+if(c>max)
+    {
+     max=c;
+    }
+ System.Console.Write($"Максимальным значением из трёх чисел является число {max}"); 
