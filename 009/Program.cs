2e28cd5e0ff71eaee5243f7c54d08589d32351bf
Dmitrian23 <dimonio1991@mail.ru>
2023-02-01 04:45:47 +0300

    Задача номер 009

---
+++ 009/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача_№8 Вывести на экран чётные числа от 1 до N.
+int i=2;
+int N;
+
+       System.Console.Write("Введите пожалуйста число для переменной(N):");
+            N=Convert.ToInt32(Console.ReadLine());
+
+while(i<=N)
+     { 
+       System.Console.WriteLine($"Чётное число ------> {i}");
+            i=i+2;
+     }
+       System.Console.WriteLine("А у нас отлично получается) Попробуй ещё раз!))");
