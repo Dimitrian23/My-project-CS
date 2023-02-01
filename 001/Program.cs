fafb1252542893d6a0e8fab18933c7eeb78ce3fc
Dmitrian23 <dimonio1991@mail.ru>
2023-02-01 04:42:28 +0300

    Задача номер 001

---
+++ 001/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача_№1 С клавиатуры вводится целое число. Вывести квадрат числа.
+checked
+{
+string? s;
+int a; 
+System.Console.WriteLine("Введите c клавиатуры число, а затем нажмите Enter)");
+     s=Console.ReadLine();
+     a=Convert.ToInt32(s);
+System.Console.WriteLine("Возводим введённое число в квадрат!");
+System.Console.WriteLine("Получаем число:");
+       Console.WriteLine (a*a);
+System.Console.WriteLine("Задача решена!Троекратное Ура!");
+}
