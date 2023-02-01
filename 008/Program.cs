a0dc2edcaad8a1346328ef9a69b7fec1ab5d6ce9
Dmitrian23 <dimonio1991@mail.ru>
2023-02-01 04:45:26 +0300

    Задача номер 008

---
+++ 008/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача_№8 Вывести на экран числа от -N до N.
+
+int n;
+       System.Console.Write("Введите пожалуйста отрицательное число для переменной(-N):");
+            n=Convert.ToInt32(Console.ReadLine());
+int i=n;
+int N;
+
+       System.Console.Write("Введите пожалуйста положительное число для переменной(N):");
+            N=Convert.ToInt32(Console.ReadLine());
+
+while(i<=N)
+     { 
+       System.Console.WriteLine($" ------> {i}");
+            i=i+1;
+     }
+       System.Console.WriteLine("Отлично,попробуй ещё раз!))");   
